75c31242384bef8ad76c6663653089d5974da308
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2022-12-22 00:39:50 +0530

    2nd Question

---
+++ cf-2.c
@@ -0,0 +1,18 @@
+//Tokitsukaze and Enhancement
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+
+    if (x % 4 == 1)
+        printf("0 A\n");
+    else if (x % 4 == 2)
+        printf("1 B\n");
+    else if (x % 4 == 3)
+        printf("2 A\n");
+    else
+        printf("1 A\n");
+    return 0;
+}
