cbb8ed5d26ffd3d2107051e3159ea20d03a59fc1
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2023-02-03 20:06:10 +0530

    110

---
+++ cf-110.cpp
@@ -0,0 +1,59 @@
+// Tram
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <memory>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <complex>
+#include <numeric>
+#include <valarray>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <accctrl.h>
+#include <math.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main()
+{
+    int n, minCap = 0, count = 0;
+    cin >> n;
+
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        count += b;
+        count -= a;
+
+        minCap = max(minCap, count);
+    }
+
+    cout << minCap;
+    return 0;
+}
