05aadbb3c558da8cb2665c1cb7e63642f295b52e
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2023-02-05 20:55:50 +0530

    112

---
+++ cf-112.cpp
@@ -0,0 +1,56 @@
+// Even Odds
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <memory>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <complex>
+#include <numeric>
+#include <valarray>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <accctrl.h>
+#include <math.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main()
+{
+    ll n, k;
+
+    cin >> n >> k;
+
+    if (k <= (n + 1) / 2)
+    {
+        cout << (k * 2) - 1;
+    }
+    else
+    {
+        cout << (k - (n + 1) / 2) * 2;
+    }
+    return 0;
+}
