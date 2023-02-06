730b9fa9f8ceeac59c14dea8ea7bf1d0cf28f306
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2023-02-06 20:19:39 +0530

    113

---
+++ cf-113.cpp
@@ -0,0 +1,50 @@
+// Dubstep
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <memory>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <complex>
+#include <numeric>
+#include <valarray>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <accctrl.h>
+#include <math.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+
+    regex r("WUB");
+
+    cout << std::regex_replace(s, r, " ");
+}
