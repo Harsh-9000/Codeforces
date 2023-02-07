6a5a0a5a2be38a5812caade2eefaaa2ab445b273
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2023-02-07 19:37:16 +0530

    114

---
+++ cf-114.cpp
@@ -0,0 +1,59 @@
+// Anton and Letters
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <memory>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <complex>
+#include <numeric>
+#include <valarray>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <accctrl.h>
+#include <math.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main()
+{
+    string letters;
+
+    set<char> alphabets;
+
+    getline(cin, letters);
+
+    for (int i = 0; i < letters.length(); i++)
+    {
+        if (letters[i] >= 'a' && letters[i] <= 'z')
+        {
+            alphabets.insert(letters[i]);
+        }
+    }
+
+    cout << alphabets.size() << endl;
+    return 0;
+}
