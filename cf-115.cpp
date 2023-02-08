e4faccba4d3c02012516dd57af2a7f75f5af2186
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2023-02-08 11:59:45 +0530

    115

---
+++ cf-115.cpp
@@ -0,0 +1,53 @@
+// Candies and Two Sisters
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <memory>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <complex>
+#include <numeric>
+#include <valarray>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <accctrl.h>
+#include <math.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << (n - 1) / 2 << endl;
+    }
+
+    return 0;
+}
