cc3d734f1795a63c8b78b2eca938d62de5e3d5f4
Harsh-9000 <113941191+Harsh-9000@users.noreply.github.com>
2023-02-12 09:26:22 +0530

    119

---
+++ cf-119.cpp
@@ -0,0 +1,60 @@
+// Amusing Joke
+#include <exception>
+#include <limits>
+#include <new>
+#include <typeinfo>
+#include <stdexcept>
+#include <utility>
+#include <functional>
+#include <memory>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+#include <complex>
+#include <numeric>
+#include <valarray>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <sstream>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <accctrl.h>
+#include <math.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main()
+{
+    string guest, host, letters, s;
+    cin >> guest >> host >> letters;
+
+    s = guest + host;
+
+    sort(s.begin(), s.end());
+    sort(letters.begin(), letters.end());
+
+    if (s == letters)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
